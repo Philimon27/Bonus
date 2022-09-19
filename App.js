ee39157ce15444f8351fb0bec47be9c0a3d1f136
Filimon Bihonegn <100728745+Philimon27@users.noreply.github.com>
2022-09-19 14:16:45 -0500

    changes

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import React from 'react';
+import { StatusBar, StyleSheet, View } from 'react-native';
+import { Amplify } from 'aws-amplify';
+import awsconfig from './src/aws-exports';
+import Home from './src/Home';
 
-export default function App() {
+
+Amplify.configure(awsconfig);
+
+ function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar />
+      <Home />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1,
   },
 });
+export default withAuthenticator(App)
